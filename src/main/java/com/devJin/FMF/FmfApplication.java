3c0ef635d68a3bfed30e5a9769bf72ad7af98d44
euijinkim <kpasd002@gmail.com>
2020-09-25 15:39:44 +0900

    first commit

---
+++ src/main/java/com/devJin/FMF/FmfApplication.java
@@ -0,0 +1,13 @@
+package com.devJin.FMF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devJin/FMF/FmfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devJin.FMF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
