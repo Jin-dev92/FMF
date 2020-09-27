c5953094ad43a9926e5a05ac6538d2e8d8d8cd57
euijinkim <kpasd002@gmail.com>
2020-09-27 16:28:19 +0900

    초기 환경 설정 및 gradle 책과 맞게 수정

--- src/test/java/com/devJin/FMF/web/HelloControllerTest.java
+++ src/test/java/com/devJin/FMF/web/HelloControllerTest.java
@@ -1,15 +1,20 @@
 package com.devJin.FMF.web;
 
-import org.junit.jupiter.api.Test;
-
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
 
 @RunWith(SpringRunner.class) // 스프링 부트 테스트와 Junit 사이에 연결자 역할을 함
 @WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션중 web에 집중 할 수 있는 어노테이션
@@ -25,4 +30,19 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello)); // mvc.perform의 결과를 검증
 
     }
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto"))
+//                .param("name",name) api 테스트할 때 사용될 요청 파라미터를 설정 , String 값만 허용
+//                .param("amount",String.valueOf(amount))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메소드 , $ 기준으로 필드명을 명시
+                .andExpect(jsonPath("$.amount",is(amount)));
+
+    }
+
+
 }
