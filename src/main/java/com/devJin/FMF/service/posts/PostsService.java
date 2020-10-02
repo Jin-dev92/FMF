5426bc26eec22c2f8c3c5b03a91ae768af105ae4
euijinkim <kpasd002@gmail.com>
2020-10-02 21:04:20 +0900

    수정 api 추가

--- src/main/java/com/devJin/FMF/service/posts/PostsService.java
+++ src/main/java/com/devJin/FMF/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.devJin.FMF.service.posts;
 
 
+import com.devJin.FMF.domain.posts.Posts;
 import com.devJin.FMF.domain.posts.PostsRepository;
+import com.devJin.FMF.web.dto.PostsResponseDto;
 import com.devJin.FMF.web.dto.PostsSaveRequestDto;
+import com.devJin.FMF.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +20,16 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional // 수정 api
+    public Long update(Long id , PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다 id = "+id));
+        return new PostsResponseDto(entity);
+    }
+
 }
--- src/main/java/com/devJin/FMF/web/PostsApiController.java
+++ src/main/java/com/devJin/FMF/web/PostsApiController.java
@@ -2,11 +2,11 @@ package com.devJin.FMF.web;
 
 
 import com.devJin.FMF.service.posts.PostsService;
+import com.devJin.FMF.web.dto.PostsResponseDto;
 import com.devJin.FMF.web.dto.PostsSaveRequestDto;
+import com.devJin.FMF.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +14,19 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){  // 등록 api
         return postsService.save(requestDto);
     }
 
+    @PutMapping("api/v1/post/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
 
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+//    public
 }
